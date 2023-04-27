d73390417fec5d4347657a8c5a2c474582e15605
Makuba17 <dmakuba15@gamil.com>
2023-04-27 07:35:49 -0700

    singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,40 +1,54 @@
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
-int _strlen_recursion(char *s);
+/**
+ * _strlen - finds the length of a string
+ * @str: string to find the length of
+ *
+ * Return: length of string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
 
 /**
- * add_node_end - adds a new node at the end of a list_t list.
- * @head: pointer to the first element of the list.
- * @str: string to set in the new node.
- * Return: address of the new element, or NULL if it failed
- **/
+ * add_node_end - adds a new node to the end of linked list
+ * @head: double pointer to a linked list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *aux = *head;
+	list_t *new, *tmp;
 
+	if (str == NULL)
+		return (NULL);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->str = strdup(str);
-	if (!new->str)
+	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->len = _strlen_recursion(new->str);
+	new->len = _strlen(new->str);
 	new->next = NULL;
-
-	if (aux)
+	if (*head == NULL)
 	{
-		while (aux->next)
-			aux = aux->next;
-		aux->next = new;
-	}
-	else
 		*head = new;
-
+		return (new);
+	}
+	tmp = *head;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = new;
 	return (new);
 }
-
